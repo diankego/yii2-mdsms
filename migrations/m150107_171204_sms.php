afacc0751c74f64bc26139e009149e9c6b916fc2
xiewulong <xiewulong@vip.qq.com>
2015-01-08 02:05:46 +0800

    migrations

---
+++ migrations/m150107_171204_sms.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Schema;
+
+class m150107_171204_sms extends \yii\db\Migration{
+
+	public function up(){
+		$tableOptions = 'engine=innodb character set utf8';
+		if($this->db->driverName === 'mysql') {
+			$tableOptions .= ' collate utf8_unicode_ci';
+		}
+
+		$this->createTable('{{%sms}}', [
+			'id' => Schema::TYPE_PK . ' comment "短信id"',
+			'uid' => Schema::TYPE_INTEGER . ' not null default 0 comment "操作者: 0系统, >0用户id"',
+			'phone' => Schema::TYPE_TEXT . ' not null comment "手机号"',
+			'content' => Schema::TYPE_STRING . ' not null comment "内容"',
+			'status' => Schema::TYPE_INTEGER . ' not null default 1 comment "发送状态"',
+			'message' => Schema::TYPE_STRING . '(50) comment "状态信息"',
+			'created_at' => Schema::TYPE_INTEGER . ' not null comment "发送时间"',
+		], $tableOptions . ' comment="短信发送记录"');
+	}
+
+	public function down(){
+		$this->dropTable('{{%sms}}');
+	}
+
+}
--- migrations/schema-mysql.sql
+++ migrations/schema-mysql.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `dkg_sms` (
-	`id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '短信id',
-	`uid` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT '操作者: 0系统, >0用户id',
-	`phone` TEXT NOT NULL COMMENT '手机号',
-	`content` VARCHAR(255) NOT NULL COMMENT '内容',
+CREATE TABLE `tbpre_sms` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '短信id',
+	`uid` INT(11) NOT NULL DEFAULT '0' COMMENT '操作者: 0系统, >0用户id',
+	`phone` TEXT NOT NULL COMMENT '手机号' COLLATE 'utf8_unicode_ci',
+	`content` VARCHAR(255) NOT NULL COMMENT '内容' COLLATE 'utf8_unicode_ci',
 	`status` INT(11) NOT NULL DEFAULT '1' COMMENT '发送状态',
-	`message` VARCHAR(50) NULL DEFAULT NULL COMMENT '状态信息',
-	`created_at` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0' COMMENT '发送时间',
+	`message` VARCHAR(50) NULL DEFAULT NULL COMMENT '状态信息' COLLATE 'utf8_unicode_ci',
+	`created_at` INT(11) NOT NULL COMMENT '发送时间',
 	PRIMARY KEY (`id`)
 )
 COMMENT='短信发送记录'
-COLLATE='utf8_general_ci'
+COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB;
